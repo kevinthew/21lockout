ac58a99a2c9e9eaaae6884edb58063e3f380e634
kwohlge <kevinwohlgenant@gmail.com>
2012-04-19 14:36:32 -0400

    fake sql upload

--- testing/datareader.py
+++ testing/datareader.py
@@ -1,35 +1,33 @@
 import csv
 
-rawplayerdata = csv.DictReader(open('testing.csv', 'rb'))
 
-dict_list = []
+def table_ripper(table_name, table_stat):
 
-for line in rawplayerdata:
-    dict_list.append(line)
+    rawplayerdata = csv.DictReader(open('testing.csv', 'rb'))
 
+    dict_list = []
 
-#use this to snag a particular value attached to a particular index with 'Player' as key
-#print dict_list[0].get('Player')
+    for line in rawplayerdata:
+        dict_list.append(line)
+    
+    dict_length = len(dict_list)
+    dict_max = dict_length
+    dict_counter = 0
 
-dict_length = len(dict_list)
-dict_max = dict_length
-dict_counter = 0
+    #return dict_lists' index number for a given player's name; see below
+    #it's CASE SENSITIVE RIGHT NOW; ie Lebron James doesn't work but LeBron James does
+    player_key = {}
 
-#return dict_lists' index number for a given player's name; see below
-#it's CASE SENSITIVE RIGHT NOW; ie Lebron James doesn't work but LeBron James does
-player_key = {}
+    while dict_counter < dict_max:
+        #print dict_counter
+        player_key[dict_list[dict_counter].get('Player')] = dict_counter
+        dict_counter = dict_counter + 1
 
-while dict_counter < dict_max:
-    #print dict_counter
-    player_key[dict_list[dict_counter].get('Player')] = dict_counter
-    dict_counter = dict_counter + 1
+    player_card = dict_list
 
-player_card = dict_list
-
-print player_card[player_key['LeBron James']].get('Height')
-
+    return player_card[player_key[table_name]].get(table_stat)
 
 
-
-
+#example
+print table_ripper('LeBron James', 'Height')
 
