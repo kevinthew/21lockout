971b7acbf99ffc103d4a6750f5ba08754a31951a
Kevin Wohlgenant <kevin.wohlgenant@gmail.com>
2012-04-20 23:54:52 -0400

    cleaned up some more code; next update to fix stat calculators and to begin building a player card class to shuffle/pull from

--- testing/player_setup.py
+++ testing/player_setup.py
@@ -1,7 +1,5 @@
 ### this is where all the rollers get the player info
+### keep these variable names until building classes
 
-user_name = 'Ty Lawson'
-opponent_name = 'Ricky Rubio'
-
-
-#simple for now; this will be bumped into a class with the other info eventually
+user_name = 'LeBron James'
+opponent_name = "Mario Chalmers"
