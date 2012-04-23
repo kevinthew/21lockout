0970e3a80fb387363f7ca238e29e42f9e904329e
Kevin <kevin.wohlgenant@gmail.com>
2012-04-23 18:06:38 -0300

    Update testing/player_setup.py

--- testing/player_setup.py
+++ testing/player_setup.py
@@ -11,10 +11,3 @@ opponent_name = raw_input("Who are you going to dunk all over? ")
 ### these aren't being used right now
 user_combat_position = 'offense' #or 'defense'
 opponent_combat_position = 'defense'
-=======
-### this is where all the rollers get the player info
-### keep these variable names until building classes
-
-user_name = 'Mario Chalmers'
-opponent_name = "Daniel Orton"
->>>>>>> 92d37aa37535db2b87c357280a60244763eb7bd7
