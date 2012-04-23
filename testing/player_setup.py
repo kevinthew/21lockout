e9788dd621e54f8ddc5ca4f4f5e1eb6b0e42839d
kwohlge <kevinwohlgenant@gmail.com>
2012-04-23 17:15:14 -0400

    ok fix this

--- testing/player_setup.py
+++ testing/player_setup.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-### player info dumped here; stat rollers grab the name from here
 
+### player info dumped here; stat rollers grab the name from here
 
 user_name = raw_input("Who are you? ")
 opponent_name = raw_input("Who are you going to dunk all over? ")
 
-
-
-
 ### these aren't being used right now
 user_combat_position = 'offense' #or 'defense'
 opponent_combat_position = 'defense'
-=======
-### this is where all the rollers get the player info
-### keep these variable names until building classes
 
-user_name = 'Mario Chalmers'
-opponent_name = "Daniel Orton"
->>>>>>> 92d37aa37535db2b87c357280a60244763eb7bd7
+
