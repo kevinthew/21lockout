5e689698d8d46460ea79e1615954e94e3b43efe9
Kevin <kevin.wohlgenant@gmail.com>
2012-04-23 18:16:24 -0300

    Update testing/player_setup.py

--- testing/player_setup.py
+++ testing/player_setup.py
@@ -5,8 +5,6 @@ user_name = raw_input("Who are you? ")
 opponent_name = raw_input("Who are you going to dunk all over? ")
 
 
-
-
 ### these aren't being used right now
 user_combat_position = 'offense' #or 'defense'
 opponent_combat_position = 'defense'
